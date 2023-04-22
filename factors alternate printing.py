62d8000301787f1c41316dd5eea043304ce139d9
Nivethaa M <90699543+Nivethaa0310@users.noreply.github.com>
2023-04-22 22:24:42 +0530

    Add files via upload

---
+++ factors alternate printing.py
@@ -0,0 +1,14 @@
+x,y=map(int,input().split())
+list1=[i for i in range(1,x+1) if x%i==0] 
+list2=[i for i in range(1,y+1) if y%i==0]
+newlist = []
+a1 = len(list1)
+a2 = len(list2)
+
+for i in range(max(a1, a2)):
+    if i < a1:
+        newlist.append(list1[i])
+    if i < a2:
+        newlist.append(list2[i])
+
+print(*newlist)
